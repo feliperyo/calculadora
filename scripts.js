d2f5c284d16ae6e57cfb5663704725ab75080705
feliperyo <ryofeliperyo@gmail.com>
2024-02-22 08:21:17 -0300

    adicionando potência e raiz aos button e ajustando css

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const adButton = document.querySelector('.adição')
 const subButton = document.querySelector('.subtração')
 const divButton = document.querySelector('.divisão')
 const multButton = document.querySelector('.multiplicação')
+const potButton = document.querySelector('.potência')
+const raizButton = document.querySelector('.raiz')
 
 function clickAd() {
     let num1 = parseInt(document.querySelector('#num1').value)
@@ -35,11 +37,24 @@ function clickMult() {
     num3.value = num1 * num2
 }
 
+function clickPot() {
+    let num1 = document.querySelector('#num1').value
+    let num2 = document.querySelector('#num2').value
+    let num3 = document.querySelector('#num3')
 
+    num3.value = Math.pow(num1, num2)
+}
 
+function clickRaiz() {
+    let num1 = document.querySelector('#num1').value
+    let num3 = document.querySelector('#num3')
 
+    num3.value = Math.sqrt(num1, 2)
+}
 
 adButton.addEventListener("click", clickAd)
 subButton.addEventListener("click", clickSub)
 divButton.addEventListener("click", clickDiv)
 multButton.addEventListener("click", clickMult)
+potButton.addEventListener("click", clickPot)
+raizButton.addEventListener("click", clickRaiz)
